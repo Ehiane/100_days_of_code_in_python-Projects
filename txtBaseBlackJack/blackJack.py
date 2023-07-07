d534e870b7b51ce3887945602f38ae7d96ab28f3
Ehiane <ehis.oigiagbe@gmail.com>
2023-07-07 20:58:42 +0100

    revamped the whole programme

--- txtBaseBlackJack/blackJack.py
+++ txtBaseBlackJack/blackJack.py
@@ -1,16 +1,4 @@
 #######-----[ 🧢🪨: Black Jack]-----########
-"""
-*Rules:
-*If the sum of your cards surpass 21, it's a bust and the player loses.
-*while the total sum of your cards is lesser than 17, you must draw another card.
-*if 17 or greater then you can submit your deck.
-* who ever's card is lesser than 21 but higher than the other player wins.
-* if both draw, no one wins the round
-* if any of the player submits a 21, that player automatically wins
-* all non-numerical cards represent 10, except an ACE(A);
-* in this version of the game, the cards are not being removed from the deck
-* They're no joker cards
-"""
 
 ## TODO instead of hard coding list, use [LIST COMPREHENSION] --Done
 ## TODO use a dictionary to hold the values of the non numericals. --Done
@@ -26,121 +14,261 @@ LOW_BOUND = 17; #while the number is lesser than this, redraw from general deck;
 User_score, Computer_score = 0,0;#setting both scores to 0
 User_deck, Computer_deck = [],[]; #initialising both deck of cards.
 
-def serve_both_cards(user_deck, computer_deck,game_deck):
-    """
-    [Void*] this function adds a random card into both decks, one at a time. 
-    """
-    #getting the random indices for their card selection.
-    user_random_choice = R.randint(0,len(game_deck) - 1);
-    computer_random_choice = R.randint(0,len(game_deck) - 1);
 
-    #adding the selections into their decks 
-    user_deck.append(game_deck[user_random_choice]);
-    computer_deck.append(game_deck[computer_random_choice]);
-    ##Note: there's a little sense of vagueness if any of the decks get a 10, as a 10 could be a Q or K, 
-            ##but for this project I wont focus too much on that, because the most important thing is the score.
 
-def serve_single_card(deck,game_deck):
+def print_rules():
+    print("#########-----[ 🧢🪨: Black Jack Rules]-----#########")
+    print("* If the sum of your cards surpasses 21, it's a bust and you lose.")
+    print("* While the total sum of your cards is less than 17, you must draw another card.")
+    print("* If the sum is 17 or greater, you can choose to stop drawing cards.")
+    print("* The player with a hand value higher than the other player wins.")
+    print("* If both players have the same hand value, it's a draw.")
+    print("* If any player has a hand value of 21, they automatically win.")
+    print("* All non-numerical cards represent 10, except for an ACE (A).")
+    print("* In this version of the game, the cards are not being removed from the deck.")
+    print("* There are no joker cards.")
+    print("####################################################")
+
+
+
+
+
+def serve_both_cards(user_deck, computer_deck, game_deck):
     """
-    [Void*] this function adds a random card into a single deck. 
+    Adds a random card to both user and computer decks from the game deck.
+
+    Args:
+        user_deck (list): The user's deck of cards.
+        computer_deck (list): The computer's deck of cards.
+        game_deck (list): The game deck of cards.
+
+    Returns:
+        None
     """
-    user_random_choice = R.randint(0,len(game_deck) - 1);    
-    deck.append(game_deck[user_random_choice]);
+    user_card = R.choice(game_deck)
+    computer_card = R.choice(game_deck)
+
+    user_deck.append(user_card)
+    computer_deck.append(computer_card)
+
+def serve_single_card(deck, game_deck):
+    """
+    Adds a random card to a single deck from the game deck.
+
+    Args:
+        deck (list): The deck to which the card will be added.
+        game_deck (list): The game deck of cards.
+
+    Returns:
+        None
+    """
+    card = R.choice(game_deck)
+    deck.append(card)
 
 def sum_up_deck(deck):
-    total = 0;
-    for value in deck:
-        total += value;
-    return total;
+    """
+    Calculates the sum of values in the deck.
+
+    Args:
+        deck (list): The deck of cards.
+
+    Returns:
+        int: The sum of the values in the deck.
+    """
+    return sum(deck)
+
 
 def deal(user_score, computer_score, game_deck):
-    while(user_score < GAME_POINT or computer_score < GAME_POINT):
-        #randomly distribute the cards to the players. --call serve card function.
-        serve_both_cards(User_deck, Computer_deck, game_deck);
-        User_score,Computer_score = sum_up_deck(User_deck), sum_up_deck(Computer_deck);
-        user_score, computer_score = User_score, Computer_score;
-        #if the sum of any of the decks are below 17, re serve the card
-        if(User_score < LOW_BOUND):
-            serve_single_card(User_deck,game_deck);
-        if(Computer_score < LOW_BOUND):
-            serve_single_card(Computer_deck, game_deck);
-        return user_score, computer_score;
+    """
+    Distributes one card at a time to the players and updates their scores.
+
+    Args:
+        user_score (int): The current score of the user.
+        computer_score (int): The current score of the computer.
+        game_deck (list): The deck of cards.
+
+    Returns:
+        int: The updated score of the user.
+        int: The updated score of the computer.
+    """
+    serve_single_card(User_deck, game_deck)
+    serve_single_card(Computer_deck, game_deck)
+
+    User_score = sum_up_deck(User_deck)
+    Computer_score = sum_up_deck(Computer_deck)
+
+    return User_score, Computer_score
 
 def print_both_decks(user_deck, computer_deck):
-    print("\n\tUser's Deck: ");
-    print("\t",user_deck);
-    print("\n\tComputer's Deck: ");
-    print("\t",computer_deck,"\n");
+    """
+    Prints both the user's and computer's decks along with the sum of each deck.
+    
+    Args:
+        user_deck (list): The user's deck of cards.
+        computer_deck (list): The computer's deck of cards.
+    
+    Returns:
+        None
+    """
+    print("\n\tUser's Deck:")
+    print(f"\t{user_deck} = {sum(user_deck)}")
+    
+    print("\n\tComputer's Deck:")
+    print(f"\t{computer_deck} = {sum(computer_deck)}\n")
+
 
 def user_view_display(user_deck, computer_deck):
-    computer_last_card = computer_deck[-1];
-    computer_deck.pop();
-    computer_deck.append("{hidden}");
+    """
+    Displays the user's deck and the computer's deck (with the first card revealed).
 
-    print();
-    loading_animation("printing user's deck ", 2);
-    print("\n\tUser's Deck: ");
-    print("\t",user_deck)
+    Args:
+        user_deck (list): The user's deck of cards.
+        computer_deck (list): The computer's deck of cards.
 
-    print();
-    loading_animation("printing computer's deck ", 2)
-    print("\n\tComputer's Deck: ");
-    print("\t",computer_deck,"\n");
-    computer_deck.pop();
-    computer_deck.append(computer_last_card); #adding it back 😂
+    Returns:
+        None
+    """
+    print()
+    loading_animation("Printing user's deck ", 2)
+    print("\n\tUser's Deck:")
+    print("\t", user_deck)
+
+    print()
+    loading_animation("Printing computer's deck ", 2)
+    print("\n\tComputer's Deck:")
+    print("\t", computer_deck, "\n") 
 
 def determine_winner(user_score, user_deck, computer_score, computer_deck):
-    #reveal computer last  card --Done in user_view_display
-    if user_score <= GAME_POINT and user_score > computer_score:
-        print(user_winner_logo);
-        print_both_decks(user_deck, computer_deck);
-    
-    elif computer_score <= GAME_POINT and computer_score > user_score:
-        print(computer_winner_logo);  
-        print_both_decks(user_deck, computer_deck);
-    
-    elif(computer_score == user_score):
-        print(draw_logo);
-        print_both_decks(user_deck, computer_deck);
+    """
+    Determines the winner based on the scores and displays the result.
 
-def stage1(user_score,computer_score,game_deck,user_deck,computer_deck):
-    user_score, computer_score = deal(user_score,computer_score,game_deck);
-    #by this time both parties have 2 cards.
-    user_view_display(user_deck, computer_deck);
-    #prompt user if they want to keep their card or draw another one?
+    Args:
+        user_score (int): The score of the user.
+        user_deck (list): The user's deck of cards.
+        computer_score (int): The score of the computer.
+        computer_deck (list): The computer's deck of cards.
 
-def stage2(user_score,user_deck,computer_score,computer_deck,game_deck, draw_card_again):
-    if(draw_card_again == "yes"):
-        cts();
-        welcome_screen();
-        user_score, computer_score = deal(user_score,computer_score, game_deck);
-        
-        loading_animation("Determining winner ", 3.5)
-        cts();
-        determine_winner(user_score,user_deck,computer_score,computer_deck);
-    elif(draw_card_again == "no"):
-        cts();
-        welcome_screen();
-        loading_animation("Determining winner ", 3.5)
-        cts();
-        determine_winner(user_score,user_deck,computer_score,computer_deck);
+    Returns:
+        None
+    """
+    user_view_display(user_deck, computer_deck)
+
+    if user_score > 21:
+        print(computer_winner_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("Computer wins! You went over 21 (bust).")
+
+    elif computer_score > 21:
+        print(user_winner_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("Congratulations! Computer went over 21 (bust). You win!")
+
+    elif user_score == 21 and computer_score != 21:
+        print(user_winner_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("Congratulations! You got 21. You win!")
+
+    elif computer_score == 21 and user_score != 21:
+        print(computer_winner_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("Computer got 21. Computer wins!")
+
+    elif user_score <= 21 and (user_score > computer_score or computer_score > 21):
+        print(user_winner_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("Congratulations! Your score is higher. You win!")
+
+    elif computer_score <= 21 and (computer_score > user_score or user_score > 21):
+        print(computer_winner_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("Computer's score is higher. Computer wins!")
+
+    elif user_score == computer_score:
+        print(draw_logo)
+        print_both_decks(user_deck, computer_deck)
+        print("It's a draw!")
+
     else:
-        print("Invalid option\nStart all over again\n");
-        initialize_game();
+        print("Error: Invalid game state.")  # This should never happen in a valid game.
+
+
+
+def stage1(user_score, computer_score, game_deck, user_deck, computer_deck):
+    """
+    Executes stage 1 of the game, where each player is initially dealt one card.
+
+    Args:
+        user_score (int): The current score of the user.
+        computer_score (int): The current score of the computer.
+        game_deck (list): The deck of cards.
+        user_deck (list): The user's deck of cards.
+        computer_deck (list): The computer's deck of cards.
+
+    Returns:
+        None
+    """
+    user_score, computer_score = deal(user_score, computer_score, game_deck)
+
+    user_view_display(user_deck, computer_deck)
+
+
+
+def stage2(user_score, user_deck, computer_score, computer_deck, game_deck):
+    """
+    Executes stage 2 of the game, where additional cards are drawn if needed.
+
+    Args:
+        user_score (int): The current score of the user.
+        user_deck (list): The user's deck of cards.
+        computer_score (int): The current score of the computer.
+        computer_deck (list): The computer's deck of cards.
+        game_deck (list): The deck of cards.
+
+    Returns:
+        None
+    """
+    while True:
+        cts()
+        welcome_screen()
+        user_view_display(user_deck, computer_deck)
+        loading_animation("Dishing out cards ", 3)
+
+        if user_score >= 21 or computer_score >= 21:
+            break
+
+        if user_score > 16:
+            draw_card_again = input("Do you want to draw another card? [yes or no]\n>> ").lower()
+            if draw_card_again == "no":
+                break
+
+        serve_single_card(User_deck, game_deck)
+        serve_single_card(Computer_deck, game_deck)
+        user_score = sum_up_deck(User_deck)
+        computer_score = sum_up_deck(Computer_deck)
+
+    cts()
+    welcome_screen()
+    loading_animation("Determining winner ", 3.5)
+    cts()
+    determine_winner(user_score, user_deck, computer_score, computer_deck)
+
 
 def initialize_game():
-    welcome_screen();
-    start_pause();
+    welcome_screen()
+    start_pause()
 
-    game_deck = create_deck_of_cards(); #see <resources.py> for implementation.
-    
-    stage1(User_score,Computer_score,game_deck,User_deck,Computer_deck);
+    game_deck = create_deck_of_cards()  # see <resources.py> for implementation.
 
-    draw_card_again = input("Do you want to draw another card?[yes or no]\n>>").lower();
+    read_rules = input("Do you want to read the rules before playing? (yes/no): ").lower()
 
-    stage2(User_score,User_deck,Computer_score,Computer_deck,game_deck,draw_card_again);
+    if read_rules == "yes":
+        print_rules()
+
+    stage1(User_score, Computer_score, game_deck, User_deck, Computer_deck)
+
+    draw_card_again = input("Do you want to draw another card? (yes/no): ").lower()
+
+    stage2(User_score, User_deck, Computer_score, Computer_deck, game_deck)
 
 
-   
-
-initialize_game();
+initialize_game()
--- txtBaseBlackJack/resources.py
+++ txtBaseBlackJack/resources.py
@@ -36,112 +36,125 @@ salutations = """
                        ░                                                                               
 """
 
-
 def cts():
     """
-    [Void] to clear the screen;
+    Clears the screen.
+    
+    Returns:
+        None
     """
-    print('\033[H\033[J');
+    print('\033[H\033[J')
 
 
 def general_pause():
     """
-    [Void] this function is similar to VScommunities implementation
-    of system("pause"). It pauses the terminal and waits for the user
-    to press any key before advancing with the program. 
+    Pauses the terminal and waits for the user to press any key before advancing with the program.
+    
+    Returns:
+        None
     """
-    pause_time = True;
+    pause_time = True
     
     if pause_time:
-        print("Press any Key to continue...");
-        input();
-    cts(); #clear the terminal.
-
+        print("Press any Key to continue...")
+        input()
+    cts()  # clear the terminal.
 
 
 def start_pause():
     """
-    [Void] very similar to {general_pause()} but for starting the game only
+    Pauses the terminal and waits for the user to press any key before starting the game.
+    
+    Returns:
+        None
     """
-    pause_time = True;
+    pause_time = True
     
     if pause_time:
-        print("Press any Key to start...");
-        input();
-   
+        print("Press any Key to start...")
+        input()
 
 
 def welcome_screen():
     """
-    [Void] prints the welcome screen for this program
+    Prints the welcome screen for the program.
+    
+    Returns:
+        None
     """
-    print(salutations,"\n",logo);
-
-
+    print(salutations, "\n", logo)
 
 
 def create_deck_of_cards():
     """
-    [Array] This function returns the deck of cards used for this game.
+    Creates and returns the deck of cards used for the game.
+    
+    Returns:
+        list: The deck of cards.
     """
-    #using list comprehension notation to generate the cards list
-    cards = [x for x in range(2,11)]; 
+    cards = [x for x in range(2, 11)]  # using list comprehension notation to generate the cards list
 
-    #inserting the special cards ACE, KING and Queen into the cards array
-    #the indices are an intentional point of mine. 
-    cards.insert(0,SPECIAL_CARDS["A"][CARD_VALUE]); 
-    cards.insert(-1,SPECIAL_CARDS["K"][CARD_VALUE]);
-    cards.insert(-1,SPECIAL_CARDS["Q"][CARD_VALUE]);
-    return cards;
+    # inserting the special cards ACE, KING, and Queen into the cards array
+    cards.insert(0, SPECIAL_CARDS["A"][CARD_VALUE])
+    cards.insert(-1, SPECIAL_CARDS["K"][CARD_VALUE])
+    cards.insert(-1, SPECIAL_CARDS["Q"][CARD_VALUE])
+    return cards
 
 
 def loading_animation(text, duration=6):
     """
-    [Void] from-->(Basic Banking app), displays a loading animation effect.
+    Displays a loading animation effect.
+    
+    Args:
+        text (str): The text to display along with the loading animation.
+        duration (float, optional): The duration of the animation. Defaults to 6.
+
+    Returns:
+        None
     """
-    start_time = time.time() # get the start time
-    end_time = start_time + duration # calculate the end time
+    start_time = time.time()  # get the start time
+    end_time = start_time + duration  # calculate the end time
     while time.time() < end_time:
         for i in range(5):
-            time.sleep(0.5) # pause for half a second
+            time.sleep(0.5)  # pause for half a second
             loading_message = f"{'.' * i}"
-            print(f"{loading_message}\r{text}", end="", flush=True) # print loading message with text below
+            print(f"{loading_message}\r{text}", end="", flush=True)  # print loading message with text below
         print("\r" + " " * 100 + "\r", end="")  # clear the line and overwrite it with 100 whitespaces
-    print(f"Done {text}!") # print a message when the animation is finished
+    print(f"Done {text}!")  # print a message when the animation is finished
+
 
 
 user_winner_logo = """
- __      __  __                                           __    __                                                                    __     
-/\ \    /\ \/\ \                           __            /\ \__/\ \                              __                                  /\ \    
-\ \ \   \ \ \ \ \    ____    __  _ __     /\_\    ____   \ \ ,_\ \ \___      __       __  __  __/\_\    ___     ___      __  _ __    \ \ \   
- \ \ \   \ \ \ \ \  /',__\ /'__`/\`'__\   \/\ \  /',__\   \ \ \/\ \  _ `\  /'__`\    /\ \/\ \/\ \/\ \ /' _ `\ /' _ `\  /'__`/\`'__\   \ \ \  
-  \ \_\   \ \ \_\ \/\__, `/\  __\ \ \/     \ \ \/\__, `\   \ \ \_\ \ \ \ \/\  __/    \ \ \_/ \_/ \ \ \/\ \/\ \/\ \/\ \/\  __\ \ \/     \ \_\ 
-   \/\_\   \ \_____\/\____\ \____\ \_\      \ \_\/\____/    \ \__\\ \_\ \_\ \____\    \ \___x___/'\ \_\ \_\ \_\ \_\ \_\ \____\ \_\      \/\_\
-    \/_/    \/_____/\/___/ \/____/\/_/       \/_/\/___/      \/__/ \/_/\/_/\/____/     \/__//__/   \/_/\/_/\/_/\/_/\/_/\/____/\/_/       \/_/
 
+██╗   ██╗███████╗███████╗██████╗     ██╗    ██╗██╗███╗   ██╗███████╗
+██║   ██║██╔════╝██╔════╝██╔══██╗    ██║    ██║██║████╗  ██║██╔════╝
+██║   ██║███████╗█████╗  ██████╔╝    ██║ █╗ ██║██║██╔██╗ ██║███████╗
+██║   ██║╚════██║██╔══╝  ██╔══██╗    ██║███╗██║██║██║╚██╗██║╚════██║
+╚██████╔╝███████║███████╗██║  ██║    ╚███╔███╔╝██║██║ ╚████║███████║
+ ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝     ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚══════╝
+                                                                    
                                                                                                         
 """
 
 computer_winner_logo = """
- __      ____                                      __                                     __    __                                                                    __     
-/\ \    /\  _`\                                   /\ \__                   __            /\ \__/\ \                              __                                  /\ \    
-\ \ \   \ \ \/\_\   ___    ___ ___   _____   __  _\ \ ,_\    __  _ __     /\_\    ____   \ \ ,_\ \ \___      __       __  __  __/\_\    ___     ___      __  _ __    \ \ \   
- \ \ \   \ \ \/_/_ / __`\/' __` __`\/\ '__`\/\ \/\ \ \ \/  /'__`/\`'__\   \/\ \  /',__\   \ \ \/\ \  _ `\  /'__`\    /\ \/\ \/\ \/\ \ /' _ `\ /' _ `\  /'__`/\`'__\   \ \ \  
-  \ \_\   \ \ \L\ /\ \L\ /\ \/\ \/\ \ \ \L\ \ \ \_\ \ \ \_/\  __\ \ \/     \ \ \/\__, `\   \ \ \_\ \ \ \ \/\  __/    \ \ \_/ \_/ \ \ \/\ \/\ \/\ \/\ \/\  __\ \ \/     \ \_\ 
-   \/\_\   \ \____\ \____\ \_\ \_\ \_\ \ ,__/\ \____/\ \__\ \____\ \_\      \ \_\/\____/    \ \__\\ \_\ \_\ \____\    \ \___x___/'\ \_\ \_\ \_\ \_\ \_\ \____\ \_\      \/\_\
-    \/_/    \/___/ \/___/ \/_/\/_/\/_/\ \ \/  \/___/  \/__/\/____/\/_/       \/_/\/___/      \/__/ \/_/\/_/\/____/     \/__//__/   \/_/\/_/\/_/\/_/\/_/\/____/\/_/       \/_/
-                                       \ \_\                                                                                                                                 
-                                        \/_/                                                                                                                                 
+
+  ██████╗ ██████╗ ███╗   ███╗██████╗ ██╗   ██╗████████╗███████╗██████╗     ██╗    ██╗██╗███╗   ██╗███████╗
+██╔════╝██╔═══██╗████╗ ████║██╔══██╗██║   ██║╚══██╔══╝██╔════╝██╔══██╗    ██║    ██║██║████╗  ██║██╔════╝
+██║     ██║   ██║██╔████╔██║██████╔╝██║   ██║   ██║   █████╗  ██████╔╝    ██║ █╗ ██║██║██╔██╗ ██║███████╗
+██║     ██║   ██║██║╚██╔╝██║██╔═══╝ ██║   ██║   ██║   ██╔══╝  ██╔══██╗    ██║███╗██║██║██║╚██╗██║╚════██║
+╚██████╗╚██████╔╝██║ ╚═╝ ██║██║     ╚██████╔╝   ██║   ███████╗██║  ██║    ╚███╔███╔╝██║██║ ╚████║███████║
+ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝      ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝     ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚══════╝
+
+                                                                                                
 """
 
 draw_logo = """
- __      ______  __                            ____                                 __     
-/\ \    /\__  _\/\ \__                        /\  _`\                              /\ \    
-\ \ \   \/_/\ \/\ \ ,_\   ____         __     \ \ \/\ \  _ __   __    __  __  __   \ \ \   
- \ \ \     \ \ \ \ \ \/  /',__\      /'__`\    \ \ \ \ \/\`'__/'__`\ /\ \/\ \/\ \   \ \ \  
-  \ \_\     \_\ \_\ \ \_/\__, `\    /\ \L\.\_   \ \ \_\ \ \ \/\ \L\.\\ \ \_/ \_/ \   \ \_\ 
-   \/\_\    /\_____\ \__\/\____/    \ \__/.\_\   \ \____/\ \_\ \__/.\_\ \___x___/'    \/\_\
-    \/_/    \/_____/\/__/\/___/      \/__/\/_/    \/___/  \/_/\/__/\/_/\/__//__/       \/_/
-                                                                                           
+ ██████╗ ██████╗  █████╗ ██╗    ██╗
+██╔══██╗██╔══██╗██╔══██╗██║    ██║
+██║  ██║██████╔╝███████║██║ █╗ ██║
+██║  ██║██╔══██╗██╔══██║██║███╗██║
+██████╔╝██║  ██║██║  ██║╚███╔███╔╝
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ 
+                                  
                                                                                            
 """
