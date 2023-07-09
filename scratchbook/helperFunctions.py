fd5bbf0e852f60c3b4308c6dab8df6ba6c11bdc6
Ehiane <ehis.oigiagbe@gmail.com>
2023-07-09 21:59:58 +0100

    made minor changes

--- scratchbook/helperFunctions.py
+++ scratchbook/helperFunctions.py
@@ -194,9 +194,9 @@ def caesar(start_text, shift_amount, cipher_direction):
 
 
 
-# #TODO-1: Import and print the logo from art.py when the program starts.
+# #-1: Import and print the logo from art.py when the program starts.
 
-# #TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+# #-4: Can you figure out a way to ask the user if they want to restart the cipher program?
 # #e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
 # #If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
 # #Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
@@ -205,7 +205,7 @@ def caesar(start_text, shift_amount, cipher_direction):
 # text = input("Type your message:\n").lower()
 # shift = int(input("Type the shift number:\n"))
 
-# #TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+# #-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
 # max_shift = len(alphabet)
 # if shift > max_shift:
 #     shift %= max_shift
