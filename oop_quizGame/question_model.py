0b99663a6d4ad25c3ee5295a7323b0b55c677f60
Ehiane <ehis.oigiagbe@gmail.com>
2023-07-13 22:35:21 +0100

    still working on oop_quizGame

---
+++ oop_quizGame/question_model.py
@@ -0,0 +1,16 @@
+"""
+REQUIRMENTS:
+
+*Should hold the following attributes: text, answer
+    e.g. {var} = {class}("{text}", "{answer}")
+"""
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text;
+        self.answer = answer;
+
+#object instantiation;
+question1 = Question("There are 26 letters in the Alphabet", "False")
+
+
