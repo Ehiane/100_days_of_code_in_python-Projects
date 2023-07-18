b5f10128e063a2b7ee28a98ec28920d4840639a1
Ehiane <ehis.oigiagbe@gmail.com>
2023-07-18 12:04:54 +0100

    updated the scoreboard.py with docstrings

--- pongGame/scoreboard.py
+++ pongGame/scoreboard.py
@@ -11,17 +11,27 @@ class ScoreBoard(Turtle):
         self.update_scoreboard();
       
     def update_scoreboard(self):
+        """
+        updates the scoreboard once its called by properly
+        displaying texts on the screen in the right position.
+        """
         self.goto(-100,200);
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
         self.goto(100,200);
         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
 
     def update_left_score(self):
+        """
+        updates the left score variable on the screen once called.
+        """
         self.l_score += 1; 
         self.clear();
         self.update_scoreboard();
 
     def update_right_score(self):
+        """
+        updates the right score variable on the screen once called.
+        """
         self.r_score += 1; 
         self.clear();
         self.update_scoreboard();
