3012269accf6cbb08c5154afde0dd653728327bd
Ehiane <ehis.oigiagbe@gmail.com>
2023-07-18 12:13:39 +0100

    added docstrings to scoreboard.py

--- snakePath/scoreboard.py
+++ snakePath/scoreboard.py
@@ -17,17 +17,29 @@ class ScoreBoard(Turtle):
         
 
     def display_score(self):
+        """
+        displays the score board once called at the center of the screen.
+        """
         self.write(arg=f"score: {self.score}",align=ALIGNMENT, font= self.font);
 
     def update_score(self):
+        """
+        increases the score by +1
+        """
         self.clear();
         self.score += 1;
         self.display_score();
 
     def end_game(self):
+        """
+        clears the screen and prints the 'game over' text
+        """
         self.clear();
         self.goto((0,0));
         self.write(arg= f"GAME OVER.\nYour score is {self.score}", align= ALIGNMENT, font= self.font);
 
     def Write(self, text):
+        """
+        my personal default writing function, with preloaded font and alignment.
+        """
         self.write(arg=text, align= ALIGNMENT, font=FONT);
