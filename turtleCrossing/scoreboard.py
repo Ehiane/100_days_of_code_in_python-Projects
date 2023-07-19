74f9946034a67dd8f777af0170bd2a7a9ca6ce50
Ehiane <ehis.oigiagbe@gmail.com>
2023-07-19 19:16:17 +0100

    final touches and some docstrings

--- turtleCrossing/scoreboard.py
+++ turtleCrossing/scoreboard.py
@@ -15,19 +15,31 @@ class Scoreboard(Turtle):
         self.display_score();
 
     def display_score(self):
+        """
+        displays the score on the screen.
+        """
         self.goto(x=-220, y=240);
         self.write(font=FONT, align="center", arg= f"Level:{self.level}")
 
 
     def update_level(self):
+        """
+        updates the player level.
+        """
         self.level += 1;
 
 
     def reset(self):
+        """
+        resets the scoreboard and displays updated score
+        """
         self.clear();
         self.display_score();
 
     def end_game(self):
+        """
+        prints the game over function
+        """
         self.clear();
         self.goto(0,0);
         self.write(font=FONT, align="center", arg= f"GAME OVER.")
