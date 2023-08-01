8265ba8f356ef5a6a118cddc07850fd476ff53e2
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-01 10:01:42 +0100

    implemented the timer mechanism

--- pomodoroApp/source_code.py
+++ pomodoroApp/source_code.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# ---------------------------- CONSTANTS/ Globals ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -11,6 +11,9 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 IMAGE = "pomodoroApp/tomato.png"
 FONT = (FONT_NAME, 35, "bold");
+A_MINUTE = 60;
+reps = 1;
+
 
 #----------------------------- Functionality ----------------------------------#
 # *1. choose task to do/perform
@@ -22,17 +25,45 @@ FONT = (FONT_NAME, 35, "bold");
 #! REPEAT PROCESS TILL YOU'RE DONE WITH YOUR TASK.
 
 
+# --------------------------- Added Functions ---------------------------------- #
+def is_even(n):
+    return True if n % 2 == 0 else False;
+
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+def start_timer():
+    #* calls function on line 33.
+    global reps;
+
+    if reps == 8: 
+        count_down(LONG_BREAK_MIN*A_MINUTE);
+    
+    if not is_even(reps): #if at the 1st, 3rd , 5th or 7th rep 
+        count_down(WORK_MIN*A_MINUTE);
+    elif is_even(reps): #if at the 2nd, 3rd or 4th
+        count_down(SHORT_BREAK_MIN * A_MINUTE);
+
+    reps += 1;
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 #& Important function that takes duration of waiting, calls particular function, passing in the args.
 def count_down(count):
     #& the .itemconfig method makes changes to a canvas element.
-    canvas.itemconfig(timer_text, text=count)
+    count_min = count // A_MINUTE;
+    count_sec = count % A_MINUTE;
+
+    if count_sec <= 9:
+        count_sec = f"0{count_sec}";
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    
+    #* Responsible for the time effect. 
     if count > 0:
         #& the "count" is the argument(s) being passed into the function.
         window.after(1000,count_down, count -1); 
@@ -59,8 +90,7 @@ canvas.create_image(100,112, image= image_obj);
 #*   positioning the text on the canvas
 timer_text = canvas.create_text(103,130, text="00:00", fill="white", font= FONT);
 
-#* calls function on line 33.
-count_down(10);
+
 
 ## Text(s)/Label(s):
 #% TIP: foreground/fg serves as the background for the specific element, like texts
@@ -69,7 +99,7 @@ timer_label = Label(text="Timer", font= FONT, foreground=PINK, bg= GREEN);
 tick = Label(text="✔️", foreground=PINK, background=GREEN);
 
 ## Button(s):
-start_button = Button(text="Start", bg= PINK) #add command
+start_button = Button(text="Start", bg= PINK, command= start_timer) #add command
 reset_button = Button(text="Reset", bg= PINK) #add command
 
 ## Positioning:
