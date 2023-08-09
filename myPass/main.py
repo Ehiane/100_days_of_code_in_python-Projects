14dc64eaaaba78a60a2ff9db726f6748f0195a7a
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-09 11:55:51 +0100

    worked on the password generator and used list comprehension.

--- myPass/main.py
+++ myPass/main.py
@@ -11,6 +11,7 @@
 
 from tkinter import * #only imports classes but not regular functions 
 from tkinter import messagebox as m;
+import random;
 
 
 # %Global(s):
@@ -18,6 +19,28 @@ LOGO = "logo.png"
 DATABASE = "data.txt";
 FONT = ("Courier",12);
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10);
+nr_symbols = random.randint(2, 4);
+nr_numbers = random.randint(2, 4);
+
+# List Comprehension.
+password_list =  [random.choice(letters) for char in range(nr_letters)];
+password_list += [random.choice(symbols) for symb in range(nr_symbols)];
+password_list += [random.choice(numbers) for num in range(nr_numbers)];
+
+
+random.shuffle(password_list);
+
+password = "";
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_prog_data():
