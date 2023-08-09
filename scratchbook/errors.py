cfc04fefb0b68c88535111d82b31e9c740cf2025
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-09 23:37:13 +0100

    understanding errors and handling exceptions.

---
+++ scratchbook/errors.py
@@ -0,0 +1,17 @@
+
+## Understaning the types of errors in python:
+# Key Error
+# Index error
+#  Type error
+# File not found
+# syntax error
+
+
+## Catching Exceptions
+# syntax:
+"""
+try: {smth that might cause an exception}
+except: {do this if there was an exception}
+else: {do this if there were no exceptions}
+finally: {do this no matter what happens}
+"""
