7ba02a1344aeabc1dc915c74d7ddf64225ecd106
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-09 13:32:14 +0100

    finished the project, the button and the entry couldnt be properly styled as it is a problem with windows, as i presume.

--- myPass/main.py
+++ myPass/main.py
@@ -1,17 +1,7 @@
-# tried to import the passwordManager folder: it worked.
-# import sys
-# import os
-
-# # Add the path to the root directory (folder of folders) to sys.path
-# root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-# sys.path.append(root_directory)
-
-# # Now you can import the module
-# from passwordGenerator.complexPasswordGenerator import random_password_generator
-
 from tkinter import * #only imports classes but not regular functions 
 from tkinter import messagebox as m;
-import random;
+from random import randint ,shuffle, choice;
+import pyperclip; #!responsible for copying to clipboard
 
 
 # %Global(s):
@@ -19,27 +9,28 @@ LOGO = "logo.png"
 DATABASE = "data.txt";
 FONT = ("Courier",12);
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# Password Generator Project
+#! List Comprehension(choice, randint, shuffle --all from random module).
 
-nr_letters = random.randint(8, 10);
-nr_symbols = random.randint(2, 4);
-nr_numbers = random.randint(2, 4);
+def generate_password():
+    letters = [chr(x) for x in range(ord('a'), ord('z')+1)] + [chr(x) for x in range(ord('A'), ord('Z')+1)] #the alphabet in lower and higher case using list comprehension.
+    numbers = [str(x) for x in range(10)] #number from 1 - 10 using list comprehension
+    symbols = [chr(x) for x in [33, 35, 36, 37, 38, 40, 41, 42, 43]] #converting ascii characters of symbols to characters using list comprehension. ;
 
-# List Comprehension.
-password_list =  [random.choice(letters) for char in range(nr_letters)];
-password_list += [random.choice(symbols) for symb in range(nr_symbols)];
-password_list += [random.choice(numbers) for num in range(nr_numbers)];
+    password_list =  [choice(letters) for _ in range(randint(8, 10))];
+    password_list += [choice(symbols) for _ in range(randint(2, 4))];
+    password_list += [choice(numbers) for _ in range(randint(2, 4))];
 
-random.shuffle(password_list);
+    shuffle(password_list);
 
-password = "";
-for char in password_list:
-  password += char;
+    password = "".join(password_list);
 
-print(f"Your password is: {password}");
+    password_input.delete(0,END);
+    password_input.insert(0,string=password); #once clicked, it will populate the entry with generated password;
+    
+    pyperclip.copy(password); #!Copy's the password to my clipboard.
+
+# print(f"Your password is: {password}");
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_prog_data():
     website,email_username,password = website_input.get(), userID_input.get(), password_input.get();
@@ -62,7 +53,7 @@ def clear_entries():
 def save_to_file(filename = DATABASE):
     user_data = save_prog_data(); 
     if user_data != None:
-        content = f"{user_data[0]} | {user_data[1]} | {user_data[2]}\n";
+        content = f"'{user_data[0]}' | '{user_data[1]}' | '{user_data[2]}'\n";
         # write to file
         with open(filename, "a") as file:
             file.write(content);
@@ -89,34 +80,46 @@ canvas.create_image(100,100, image= logo_obj);
 
 ##Label(s):
 website_label = Label(text="Website:",font= FONT);
-userID_label = Label(text="Email/Username:", font=FONT);
+userID_label = Label(text="Email:", font=FONT);
 password_label = Label(text="Password:",font= FONT);
 
 ##Entry(s):
 website_input = Entry(width=35,justify="left");
 userID_input = Entry(width=35,justify="left");
-password_input= Entry(width=21,justify="left");
-userID_input.insert(0, "ehis.oigigagbe@gmail.com") #*END is a constant that represents the very last character of the entry, 0 is the beginnig
+password_input= Entry(width=21, justify="left");
+userID_input.insert(0, "user@email.com") #*END is a constant that represents the very last character of the entry, 0 is the beginnig
 
 website_label.focus(); #! Sets the cursor to this entry at the start of the program.
 
 
 ##Button(s):
-generate_password_button = Button(text="Generate Password"); #add command
+generate_password_button = Button(text="Generate Password", command=generate_password); #add command
 add_button= Button(text="Add", width=36,command=save_to_file); #add command
 
 
-##Element positioning:
+## Element positioning:
 canvas.grid(column=1, row=0);
 website_label.grid(column=0, row=1);
 website_input.grid(column=1, row=1, columnspan=2);
 userID_label.grid(column=0, row=2);
 userID_input.grid(column=1, row=2, columnspan=2);
-password_input.grid(column=1, row=3);
-password_label.grid(column=0,row=3);
-generate_password_button.grid(column=2,row=3);
+password_label.grid(column=0, row=3);
+password_input.grid(column=1, row=3);  # Update the grid position
+generate_password_button.grid(column=2, row=3);
 add_button.grid(column=1, row=4, columnspan=2);
 
 
-
 window.mainloop();
+
+"""
+# tried to import the passwordManager folder: it worked.
+# import sys
+# import os
+
+# # Add the path to the root directory (folder of folders) to sys.path
+# root_directory = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# sys.path.append(root_directory)
+
+# # Now you can import the module
+# from passwordGenerator.complexPasswordGenerator import random_password_generator
+"""
