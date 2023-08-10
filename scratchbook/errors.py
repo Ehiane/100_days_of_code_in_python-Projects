e6a7d48d90a273d51f18d71f053bca43a87e359e
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-10 07:41:39 +0100

    Learning errors in python

--- scratchbook/errors.py
+++ scratchbook/errors.py
@@ -15,3 +15,20 @@ except: {do this if there was an exception}
 else: {do this if there were no exceptions}
 finally: {do this no matter what happens}
 """
+
+# try:
+#     file = open("afile.txt");
+# except:
+#     file = open("afile.txt","w");
+
+#@ Specifiying the type of exception.
+# if you're not specific with your "except" error datatype,
+# then if there is an error in one of the "try" blocks, it'' default to the "except" class
+
+try:
+    file = open("afile.txt");
+    a_dictionary = {"key":"value"};
+    print(a_dictionary["ghg"]);
+except FileNotFoundError:
+    file=open("afile.txt","w");
+    file.write("smth")
