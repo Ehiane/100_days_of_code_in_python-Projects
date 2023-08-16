714790f26ddd95ec2c5763f5cf9718d09a2cb05c
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-16 13:12:32 +0100

    adding subject

--- automaticBirthdayWisher/script.py
+++ automaticBirthdayWisher/script.py
@@ -12,7 +12,11 @@ connection = smtplib.SMTP("smtp.gmail.com");
 #* makes it impossible for the mail to be understood if intercepted by a third party.
 connection.starttls(); #basically makes the connection secure.
 connection.login(user = my_email, password = password_)
-connection.sendmail(from_addr = my_email, to_addrs = recepient_email, msg="Testing from python.");
+connection.sendmail(
+    from_addr = my_email, 
+    to_addrs = recepient_email, 
+    msg="Subject:Hello\n\nThis is the body of my mail."
+    );
 connection.close();
 
 
