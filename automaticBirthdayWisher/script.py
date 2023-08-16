8f85770bc41d8e056445111c89217d731c9baced
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-16 12:54:55 +0100

    just learnt how to send email's using python

--- automaticBirthdayWisher/script.py
+++ automaticBirthdayWisher/script.py
@@ -2,9 +2,9 @@ import smtplib;
 # smtplib.SMTP("smtp.gmail,com",port=587);
 
 my_email = "ehianesocials@gmail.com";
-password_ = "abcd1234()"
+password_ = "ouyzzwqnznprrghu"
 recepient_email = "ehiane.business@gmail.com";
-connection = smtplib.SMTP("smptp.gmail.com");
+connection = smtplib.SMTP("smtp.gmail.com");
 
 # TLS- Transport Layer Security.
 #* makes it impossible for the mail to be understood if intercepted by a third party.
@@ -12,3 +12,5 @@ connection.starttls(); #basically makes the connection secure.
 connection.login(user = my_email, password = password_)
 connection.sendmail(from_addr = my_email, to_addrs = recepient_email, msg="Testing from python.");
 connection.close();
+
+
