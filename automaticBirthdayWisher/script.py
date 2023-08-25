bb37011e085b977bb4d0c9f1b83ad0bde1f93fd5
Ehiane <ehis.oigiagbe@gmail.com>
2023-08-25 09:42:39 -0700

    introduced to date time module

--- automaticBirthdayWisher/script.py
+++ automaticBirthdayWisher/script.py
@@ -1,4 +1,6 @@
 import smtplib;
+import datetime as dt;
+
 # smtplib.SMTP("smtp.gmail,com",port=587);
 
 ## MESSAGES WITH NO SUBJECT ARE PRIMED AS TARGET FOR SPAM MAILS. 
@@ -11,29 +13,20 @@ recepient_email = "ehiane.business@gmail.com";
 # connection = smtplib.SMTP("smtp.gmail.com");
 
 # # TLS- Transport Layer Security.
-# #* makes it impossible for the mail to be understood if intercepted by a third party.
-# connection.starttls(); #basically makes the connection secure.
-# connection.login(user = my_email, password = password_)
-# connection.sendmail(
-#     from_addr = my_email, 
-#     to_addrs = recepient_email, 
-#     msg="Subject:Hello\n\nThis is the body of my mail."
-#     );
-# connection.close();
-
-
 # A better way:
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls(); #basically makes the connection secure.
-    connection.login(user = my_email, password = password_)
-    connection.sendmail(
-        from_addr = my_email, 
-        to_addrs = recepient_email, 
-        msg="Subject:Hello\n\nThis is the body of my mail."
-    );
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls(); #basically makes the connection secure.
+#     connection.login(user = my_email, password = password_)
+#     connection.sendmail(
+#         from_addr = my_email, 
+#         to_addrs = recepient_email, 
+#         msg="Subject:Hello\n\nThis is the body of my mail."
+#     );
 
 
 # hi again
 # school stuff started slowing down. 
 
+today = dt.datetime.now()
+print("Todays date : ",today);
 
