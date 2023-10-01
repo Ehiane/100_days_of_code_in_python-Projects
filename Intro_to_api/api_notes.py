9d57ac66bda13d0abba0d0ce61744cfa4026a376
Ehiane <ehis.oigiagbe@gmail.com>
2023-10-01 07:05:00 -0700

    edited some stuff

---
+++ Intro_to_api/api_notes.py
@@ -0,0 +1,38 @@
+
+## API stands for Application Programming Interface
+"""
+An API is basically a resource package, that could be a set of commands, functions,
+protocols and objects that is used to create software or interact with 
+an external system.
+
+#Simple Analogy:
+lets say you go to a restaurant.
+Your tummy is the software you are building, the menu is the API because it has all the information of the food you want to get
+the Kitchen is the external system that makes the food. your request is your order, it's response is your food.
+---
+it's like a bridge between your program and external system. 
+You request smth(provided that you follow the rules) and it sends a response.  
+
+#most companies have their own api
+
+#API endpoints
+think about it as a location. If you wanted to get money out of a bank, 
+you'd need to know what that bank is and what is its address.
+
+that address in API lingo is the API endpoint. It is usually just a url
+
+#API requests
+imagine going to a bank and you want to withdraw money out. Thats an API request. 
+basically requesting for external data
+
+#difference between API's and Middleware
+Middleware acts as an intermediary layer between different software applications and componenets to provide services
+and functions that facilitate communication, data exchange and interaction between applications.
+examples are databases, servers, etc. 
+
+Middleware can use an API to accomplish its task. so An Api can also be described as Functionality of Middleware to obtain data from external systems.
+
+so get request is a simple api request that usually requires no authentication.
+
+"""
+
---
+++ Intro_to_api/main.py
@@ -0,0 +1,5 @@
+
+import requests
+
+#& the api endpoint(address of data you want to get) goes into the url variable
+# requests.get(url=)
