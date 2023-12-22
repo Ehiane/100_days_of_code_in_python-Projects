163e8ca70072b43498cd43588e691f2640ec898d
Ehiane <e.oigiagbe@wsu.edu>
2023-12-22 05:46:12 -0800

    Unescaped some of the text returned from the API to make it more readable

--- ApiQuestionaire/main.py
+++ ApiQuestionaire/main.py
@@ -3,15 +3,12 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 
-
-
 question_bank = []
 for question in question_data:
     question_text = question["question"]
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
 
 quiz = QuizBrain(question_bank)
 
@@ -20,4 +17,5 @@ while quiz.still_has_questions():
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
- 
+
+
