e654faf3521411abd2a9f75b5862ef5d069ba10b
Ehiane <e.oigiagbe@wsu.edu>
2024-01-04 11:48:40 -0800

    making graph params.

--- HabitTracker/main.py
+++ HabitTracker/main.py
@@ -1,15 +1,31 @@
 import requests,os
 
+USERNAME = 'ehiane'
+TOKEN = os.environ.get("PIXELA_USER_TOKEN")
+
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 user_params = {
-    'token': os.environ.get("PIXELA_USER_TOKEN"),
-    'username': 'ehiane',
+    'token': TOKEN,
+    'username': USERNAME,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes',
     # 'thanksCode': ..., #optional
 }
 
-
+## Created account by calling this(don't call it more than once)
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # print(response.text)
+
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+graph_params = {
+    'id':...,
+    'name':...,
+    'unit':...,
+    'type':...,
+    'color':...,
+}
+
+requests.post()
+
