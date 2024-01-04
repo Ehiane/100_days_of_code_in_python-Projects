5d9b309b2c7524cad003b30e6f4365797e765793
Ehiane <e.oigiagbe@wsu.edu>
2024-01-04 11:26:42 -0800

    day 37

---
+++ HabitTracker/notes.py
@@ -0,0 +1,11 @@
+
+
+"""
+More about HTTP Requests
+GET (requests.get(<params>?)) - collects data based on the parameters
+POST (requests.post()) - posts data  
+PUT (requests.put()) - updates existing data
+DELETE (requests.delete()) - deletes data
+
+key: '?' optional
+"""
