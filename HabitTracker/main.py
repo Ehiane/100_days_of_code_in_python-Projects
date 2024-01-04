3e568e2e53f1b205378f7f0fe55012f78e78dcef
Ehiane <e.oigiagbe@wsu.edu>
2024-01-04 12:24:09 -0800

    code cleanup

--- HabitTracker/main.py
+++ HabitTracker/main.py
@@ -19,8 +19,9 @@ user_params = {
 
 
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+GRAPH_ID = "ehianegraph1"
 graph_params = {
-    'id':"ehianegraph1",
+    'id': GRAPH_ID,
     'name':"Coding Graph",
     'unit':"hour",
     'type':"int",
@@ -31,12 +32,23 @@ headers = {
     'X-USER-TOKEN':TOKEN,
 }
 
-
 ## Created graph by calling this(don't call it more than once)
 # response = requests.post(url=GRAPH_ENDPOINT,json=graph_params, headers= headers)
 # print(response.text)
 
-## the result:
+## the result of the graph post:
 MY_GRAPH_URL = f"https://pixe.la/v1/users/ehiane/graphs/ehianegraph1.html" #click this to view my graph
+
+# a pixel is a cell on the graph
+pixel_params = {
+    'date': '20240104', # has to be in the format yyyyMMdd
+    'quantity': '1', # since  i specified mine to be in int, it has to be str(int)
+}
+
+PIXEL_CREATION_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+response = requests.post(url=PIXEL_CREATION_ENDPOINT,json=pixel_params,headers=headers)
+print(response.text) 
+
+
 
 
